5a0f1a75604d2d7647b6c5dcc68ef8b0b1a274a9
luan0ap <luanap.kun@gmail.com>
2018-11-27 15:01:23 -0200

    remove functions, vars, add functional programming

--- contentscript.js
+++ contentscript.js
@@ -1,49 +1,44 @@
 /* Copyright 2017 Abner Soares Alves Junior
 
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Apache License, Version 2.0 (the 'License');
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an 'AS IS' BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-function lookup(selection) {
-    var selectionText = encodeURI(selection.selectionText);
-    chrome.tabs.create({
-        "url": "http://www.urbandictionary.com/define.php?term=" + selectionText
-    });
-    return true;
-}
 
-function lookupInjected() {
+const newChromeTab = (...conf) => chrome.tabs.create.apply(this, conf)
+
+const lookup = (selectionText) => 
+  newChromeTab({
+    'url': 'http://www.urbandictionary.com/define.php?term=' + encodeURI(selectionText)
+  })
+
+const lookupInjected = ()  => {
     chrome.tabs.executeScript(null, {
-        code: 'var text = ""; if (window.getSelection) { text = window.getSelection().toString(); } else if (document.selection && document.selection.type != "Control") { text = document.selection.createRange().text; } { selectionText: text }'
-    }, function(results) {
-        lookup({ selectionText: results })
-    })
+        code: `const text = getSelection ? getSelection().toString() 
+                              : (selection && selection.type !== 'Control') ? selection.createRange().text : ''`
+    }, lookup)
 }
 
-chrome.commands.onCommand.addListener(function(command){
-    if(command === "urban-lookup") {
-        lookupInjected()
-    }
-});
+chrome.commands.onCommand.addListener(command => command === 'urban-lookup'? lookupInjected(): false )
 
 chrome.contextMenus.onClicked.addListener(lookup);
 chrome.browserAction.onClicked.addListener(lookupInjected);
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create({
-        "id": "urbanDictionaryContext",
-        "title": "Lookup the selected text on Urban Directory",
-        "contexts": ["selection"]
+        'id': 'urbanDictionaryContext',
+        'title': 'Lookup the selected text on Urban Directory',
+        'contexts': ['selection']
     }, function () {
         if (chrome.extension.lastError) {
-            console.error("Got expected error: " + chrome.extension.lastError.message);
+            console.error('Got expected error: ' + chrome.extension.lastError.message);
         }
     });
 });
